bf6b5d95adad05c02ba9c1fc0792f4d135ae5819
Tomiwa Ademidun <tomiwa@proteinqure.com>
2020-02-19 18:01:59 -0500

    added conda environment

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # deep-learning-life-sciences-code
 Code Samples for the book: Deep Learning for the Life Sciences
+
+## Installation
+
+`conda activate condaenv` 
--- chapter3.py
+++ chapter3.py
@@ -1,2 +1,16 @@
 import deepchem as dc
 import numpy as np
+
+
+def run():
+    # x =  4 random arrays, each array has 5 elements
+    # y = 1 random array with 4 elements
+    x = np.random.random((4, 5))
+    y = np.random.random((1, 4))
+
+    print('x', x)
+    print('y', y)
+
+
+if __name__ == '__main__':
+    run()
