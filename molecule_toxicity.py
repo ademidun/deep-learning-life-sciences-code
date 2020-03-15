0103619a141fdad9b7741f3a2cb79150585e8a6f
ademidun <tomiademidun@gmail.com>
2020-03-14 20:07:04 -0400

    add notes and comments to predicting molecule toxicity and evaluate dataset using mean ROC AUC

--- molecule_toxicity.py
+++ molecule_toxicity.py
@@ -34,21 +34,52 @@ def run():
     #  Loss function won't have systematic preference for one class
     print(transformers)
 
-def train_model(train_dataset):
+    train_model(train_dataset, test_dataset, transformers)
+
+def train_model(train_dataset, test_dataset, transformers):
     """
-    Train the model using a multitask classifier because there are multiple labels for each sample
+    Train the model using a multitask classifier because there are multiple outputs for each sample
+    and evaluate model using the mean ROC AUC.
     :param train_dataset:
     :type train_dataset:
+    :param transformers:
+    :type transformers:
     :return:
     :rtype:
     """
 
+    # this model builds a fully connected network (an MLP)
+    #  since we have 12 assays we're testing for, being able to map to multiple outputs is ideal
     # layer_sizes means that we have one hidden layer which has a width of 1,000
-    model = dc.models.MultitaskClassifier(n_tasks=12, n_features=1024, layer_sizes=[100])
+    model = dc.models.MultitaskClassifier(n_tasks=12, n_features=1024, layer_sizes=[1000])
 
     # nb_epoch means that we will divide the data into batches, and do one step of gradient descent for each batch
     model.fit(train_dataset, nb_epoch=10)
 
+    # how do we know how accurate our model is? we will find the mean ROC AUC score across all tasks
+
+    # What is an ROC AUC score? We are trying to predict the toxicity of the molecules,
+    # Receiver Operating Characteristic, Area Under Curve
+    # If there exists any threshold value where, the true positive rate is 1 and false positive is 0 then score is 1
+    # so we pick a threshold of what is considered a toxic molecule
+    # if we pick a threshold value that's too low, we will say too many safe molecules are toxic (high false positive)
+    # alternatively, if we pick one too igh, we will say that toxic molecules are safe (high false negative)
+    # note on understanding false positive terminology.\:
+    # Imagine a molecule that is actually toxic. "Is this molecule toxic?" "No." We gave a negative response
+    # the answer is relative to what we are testing for, in this case, we are testing if a molecule is toxic
+    # so we are making a tradeoff between high false positive vs high false negative so we use something called
+    # an ROC AUC curve, which graphs the tradeofff between the false positive rate and the true positive rate
+
+    metric = dc.metrics.Metric(dc.metrics.roc_auc_score, np.mean)
+
+    # evaluate the performance of this model on the train_dataset using the ROC AUC metric
+
+    train_scores = model.evaluate(train_dataset, [metric], transformers)
+    test_scores = model.evaluate(test_dataset, [metric], transformers)
+
+    # the train scores are higher than our test scores which shows us that our model has been overfit
+    print(f'train_scores: {train_scores}')
+    print(f'test_scores: {test_scores}')
 
 if __name__ == '__main__':
     run()
--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-
+export PATH="/opt/anaconda3/bin:$PATH"
+conda init bash
 conda activate ./condaenv
+
+# to go back to normal $PATH
+# source ~/.bash_profile
